4ef019059f053c5ed1b1b170f3bef5eea4e470cd
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2023-04-23 17:07:06 +0530

    getting started and seting up chakra ui

---
+++ src/chakra/theme.ts
@@ -0,0 +1,26 @@
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css";
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    100: "#ff3c00",
+  },
+};
+
+const fonts = {
+  body: "Open Sans, sans-serif",
+};
+
+const styles = {
+  global: {
+    body: {
+      bg: "gray.200",
+    },
+  },
+};
+
+const components = {};
+
+export const theme = extendTheme({ colors, fonts, styles, components });

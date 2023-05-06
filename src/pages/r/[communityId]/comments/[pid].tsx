698dddd4ba05d79318ebe773017ed3f231c804bf
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2023-05-06 22:34:17 +0530

    add: single post page

---
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -0,0 +1,67 @@
+import { useEffect } from "react";
+import PageContent from "@/src/components/Layout/PageContent";
+import PostItem from "@/src/components/Posts/PostItem";
+import { auth, firestore } from "@/src/firebase/clientApp";
+
+import usePosts from "@/src/hooks/usePosts";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+import { doc, getDoc } from "firebase/firestore";
+import { Post } from "@/src/atoms/postsAtom";
+import About from "@/src/components/Community/About";
+import useCommunityData from "@/src/hooks/useCommunityData";
+
+const PostPage = () => {
+  const [user] = useAuthState(auth);
+  const { postStateValue, setPostStateValue, onVote, onDeletePost } =
+    usePosts();
+  const { communityStateValue } = useCommunityData();
+  const router = useRouter();
+
+  const fetchPost = async (postId: string) => {
+    try {
+      const postDocRef = doc(firestore, "posts", postId);
+      const postDoc = await getDoc(postDocRef);
+      setPostStateValue((prev) => ({
+        ...prev,
+        selectedPost: { id: postDoc.id, ...(postDoc.data() as Post) },
+      }));
+    } catch (error) {
+      console.log("fetchPost error", error);
+    }
+  };
+
+  useEffect(() => {
+    const { pid } = router.query;
+    if (pid && !postStateValue.selectedPost) {
+      fetchPost(pid as string);
+    }
+  }, [router.query, postStateValue.selectedPost]);
+
+  return (
+    <PageContent>
+      <>
+        {postStateValue.selectedPost && (
+          <PostItem
+            post={postStateValue.selectedPost}
+            onVote={onVote}
+            onDeletePost={onDeletePost}
+            userVoteValue={
+              postStateValue.postVotes.find(
+                (item) => item.postId === postStateValue.selectedPost?.id
+              )?.voteValue
+            }
+            userIsCreator={user?.uid === postStateValue.selectedPost?.creatorId}
+          />
+        )}
+      </>
+      <>
+        {communityStateValue.currentCommunity && (
+          <About communityData={communityStateValue.currentCommunity} />
+        )}
+      </>
+    </PageContent>
+  );
+};
+
+export default PostPage;
